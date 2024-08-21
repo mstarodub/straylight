3732147e496624ddebf62be2b5fc6bee5ce32fa3
mxs <mxs@dismail.de>
2024-08-22 00:58:01 +0200

    silence cabal warning about missing home modules

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,14 @@
+{-# OPTIONS_GHC -Wno-unused-imports #-}
+
 module Main where
+
+import Core
+import Elab
+import Order
+import Parse
+import Pretty
+import Superpos
+import Unif
 
 main :: IO ()
 main = pure ()
